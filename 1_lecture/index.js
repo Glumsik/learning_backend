843c3a570c4b77baf3173141b4ad536e6a76ded5
Nikita Tsapaev <n.tsapaev@onpoint.ru>
2021-07-29 01:48:55 +0300

    add 1 lecture

---
+++ 1_lecture/index.js
@@ -0,0 +1,87 @@
+#!/usr/bin/env node
+const yargs = require("yargs/yargs");
+const { hideBin } = require("yargs/helpers");
+
+const argv = yargs(hideBin(process.argv)).argv;
+const argvKeys = Object.keys(argv);
+
+const fullOptions = {
+  day: "numeric",
+  month: "long",
+  year: "numeric",
+  weekday: "long",
+  hour: "numeric",
+  minute: "numeric",
+  second: "numeric",
+};
+
+const yearOptions = {
+  year: "numeric",
+};
+
+const monthOptions = {
+  month: "long",
+};
+
+const dayOptions = {
+  day: "numeric",
+};
+
+const date = new Date();
+
+const getDate = {
+  year: () => {
+    return date.toLocaleString("ru", yearOptions);
+  },
+  y: () => {
+    return date.toLocaleString("ru", yearOptions);
+  },
+  month: (arr, number) => {
+    if (!!arr.length && arr.includes("sub")) {
+      date.setMonth(date.getMonth() - number);
+      return date.toLocaleString("ru", fullOptions);
+    } else {
+      return date.toLocaleString("ru", monthOptions);
+    }
+  },
+  m: (arr, number) => {
+    if (!!arr.length && arr.includes("sub")) {
+      date.setMonth(date.getMonth() - number);
+      return date.toLocaleString("ru", fullOptions);
+    } else {
+      return date.toLocaleString("ru", monthOptions);
+    }
+  },
+  date: (arr, number) => {
+    if (!!arr.length && arr.includes("add")) {
+      date.setDate(date.getDate() + number);
+      return date.toLocaleString("ru", fullOptions);
+    } else {
+      return date.toLocaleString("ru", dayOptions);
+    }
+  },
+  d: (arr, number) => {
+    console.log(arr, number);
+    if (!!arr.length && arr.includes("add")) {
+      date.setDate(date.getDate() + number);
+      return date.toLocaleString("ru", fullOptions);
+    } else {
+      return date.toLocaleString("ru", dayOptions);
+    }
+  },
+  full: () => {
+    return date.toLocaleString("ru", fullOptions);
+  },
+};
+
+// console.log(argv);
+
+argvKeys.length > 2
+  ? (() => {
+      argvKeys.map((item) => {
+        getDate[item] && console.log(getDate[item](argv["_"], argv[item]));
+      });
+    })()
+  : (() => {
+      console.log(getDate["full"]());
+    })();
