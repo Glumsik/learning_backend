1278ea30a89092fcc3e443d198d5a089511e6e2b
Nikita Tsapaev <n.tsapaev@onpoint.ru>
2021-08-10 00:22:50 +0300

    add 2_lecture

--- 2_lecture/index.js
+++ 2_lecture/index.js
@@ -1,12 +1,33 @@
 const readline = require("readline");
-
 const input = readline.createInterface(process.stdin);
 
-// console.log(
-//   "выберите сложность (введите цифру)\n1. легкий уровень\n2. средний уровень\n3. сложный уровень"
-// );
+const getRandomInt = (max) => {
+  const minNumber = Math.floor(Math.random() * max);
+  const maxNumber = minNumber + Math.floor(Math.random() * max);
+  const number =
+    Math.floor(Math.random() * (maxNumber - minNumber)) + minNumber;
+  return [minNumber, maxNumber, number];
+};
+
+const [minNumber, maxNumber, number] = getRandomInt(100);
+let countTry = 0;
+
+console.log(`Я загадал число от ${minNumber} до ${maxNumber}, отгадай его`);
 
 input.on("line", (data) => {
-  if (data === "close") process.exit();
-  console.log(data);
+  let inputNumber = Number(data);
+  if (data === "close") {
+    console.log(
+      "Ты потратил: " + countTry + " попытки, но так и не отгадал число"
+    );
+    process.exit(0);
+  } else if (inputNumber === number) {
+    console.log("Ты выиграл на: " + countTry + " попытке");
+    process.exit(0);
+  } else if (inputNumber > number) {
+    console.log("Меньше");
+  } else if (inputNumber < number) {
+    console.log("Больше");
+  }
+  countTry++;
 });
