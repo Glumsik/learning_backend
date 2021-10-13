023151cbb016ac76bda1295566f69c62410f62d5
Nikita Tsapaev <n.tsapaev@onpoint.ru>
2021-10-14 00:51:35 +0300

    add 5 lecture

--- 5_lecture/index.js
+++ 5_lecture/index.js
@@ -22,22 +22,22 @@ app.post("/api/user/login", (_, res) => {
   res.status(201).send({ id: 1, mail: "test@mail.ru" });
 });
 
-app.get("/api/books", async (_, res) => {
+const getAllBooks = async (_, res) => {
   const allBooks = await readFile(booksPath);
   res.send(allBooks || "не смог получить книги");
-});
+};
 
-app.get("/api/books/:id", async (req, res) => {
+const getBook = async (req, res) => {
   const { id } = req.params;
   const allBooks = await readFile(booksPath);
   const curentBook = allBooks.filter((book) => book["id"] === id);
-  console.log(curentBook);
+
   !!curentBook.length
     ? res.status(200).send(curentBook)
     : res.status(404).send("error\n книга не найдена");
-});
+};
 
-app.post("/api/books", async (req, res) => {
+const createBook = async (req, res) => {
   if (Object.keys(req.body).length == 0) {
     res.status(404).send("пустые данные");
   } else {
@@ -45,12 +45,13 @@ app.post("/api/books", async (req, res) => {
     const nextId = allBooks.length + 1;
     const newBook = { id: `${nextId}`, ...defaultBook, ...req.body };
     allBooks.push(newBook);
+
     await writeFile(booksPath, allBooks);
     res.status(200).send(JSON.stringify(newBook) + " книга записана");
   }
-});
+};
 
-app.put("/api/books/:id", async (req, res) => {
+const updateBook = async (req, res) => {
   if (Object.keys(req.body).length === 0) {
     res.status(404).send("пустые данные");
   } else {
@@ -66,20 +67,7 @@ app.put("/api/books/:id", async (req, res) => {
     await writeFile(booksPath, allBooks);
     res.status(200).send(JSON.stringify(allBooks[bookId]) + " книга изменена");
   }
-});
-
-// app.delete("/api/books/:id", async (req, res) => {
-//   const { id } = req.params;
-
-//   const allBooks = await readFile(booksPath);
-//   const newBooks = allBooks.filter((book) => book.id !== `${id}`);
-
-//   if (allBooks.length === newBooks.length)
-//     return res.status(404).send("книга по такому id не найдена");
-
-//   await writeFile(booksPath, newBooks);
-//   res.status(200).send("книга по такому id удалена");
-// });
+};
 
 const deleteBooks = async (req, res) => {
   const { id } = req.params;
@@ -94,4 +82,9 @@ const deleteBooks = async (req, res) => {
   res.status(200).send("книга по такому id удалена");
 };
 
-router.delete("/:id", deleteBooks);
+router
+  .get("/", getAllBooks)
+  .get("/:id", getBook)
+  .post("/", createBook)
+  .put("/:id", updateBook)
+  .delete("/:id", deleteBooks);
